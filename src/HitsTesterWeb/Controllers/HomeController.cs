68cecd4b97467d34dd98f1e6ecb1aa30f189c290
Roelf-Jan de Vries <roelfjan@q42.nl>
2020-04-30 20:00:25 +0200

    Hitstester - Improved UI a bit

--- src/HitsTesterWeb/Controllers/HomeController.cs
+++ src/HitsTesterWeb/Controllers/HomeController.cs
@@ -8,12 +8,14 @@ namespace HitsTesterWeb.Controllers
 	public class HomeController : Controller
 	{
 		[HttpGet]
+		[Route("/")]
 		public IActionResult Index()
 		{
 			return View();
 		}
 
 		[HttpPost]
+		[Route("/")]
 		public IActionResult IndexPost(FormModel model)
 		{
 			var hits = HaagsTranslator.Translator.GetHits(model.Text).Where(h => h.Item2).Select(t => t.Item1).ToList();
